e14a0e88db6f87f34c2e09a964b1e166dcc0eae7
Javier Peluso <jpelusocoderhouse@gmail.com>
2022-06-24 23:16:18 -0300

    update scss

--- js/script.js
+++ js/script.js
@@ -1,43 +1,45 @@
-	// BOTON RESPONSIVE
-
+// BOTON RESPONSIVE
 	document.querySelector('.menu-btn').addEventListener('click',() => {
 		document.querySelector('.nav-menu').classList.toggle('show');
 	});
 
+	
+//TIENDA CURSOS
+
 $(document).ready(function(){
 
 	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE
-	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+	$('.listaCategoria .listaCursos[category="all"]').addClass('ct_item-active');
 
 	// FILTRANDO PRODUCTOS 
 
-	$('.category_item').click(function(){
+	$('.listaCursos').click(function(){
 		let catProduct = $(this).attr('category');
 		console.log(catProduct);
 
 		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
-		$('.category_item').removeClass('ct_item-active');
+		$('.listaCursos').removeClass('ct_item-active');
 		$(this).addClass('ct_item-active');
 
 		// OCULTANDO PRODUCTOS 
-		$('.product-item').css('transform', 'scale(0)');
+		$('.tipoCurso').css('transform', 'scale(0)');
 		function hideProduct(){
-			$('.product-item').hide();
+			$('.tipoCurso').hide();
 		} setTimeout(hideProduct,400);
 
 		// MOSTRANDO PRODUCTOS 
 		function showProduct(){
-			$('.product-item[category="'+catProduct+'"]').show();
-			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+			$('.tipoCurso[category="'+catProduct+'"]').show();
+			$('.tipoCurso[category="'+catProduct+'"]').css('transform', 'scale(1)');
 		} setTimeout(showProduct,400);
 	});
 
 	// MOSTRANDO TODOS LOS PRODUCTOS
 
-	$('.category_item[category="all"]').click(function(){
+	$('.listaCursos[category="all"]').click(function(){
 		function showAll(){
-			$('.product-item').show();
-			$('.product-item').css('transform', 'scale(1)');
+			$('.tipoCurso').show();
+			$('.tipoCurso').css('transform', 'scale(1)');
 		} setTimeout(showAll,400);
 	});
 });
